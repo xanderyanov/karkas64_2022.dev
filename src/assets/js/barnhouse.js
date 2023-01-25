b61c502ad92df6862bc790bfd63928b1a1486f65
xanderyanov <xanderyanov@gmail.com>
2023-01-25 08:54:23 +0400

    8-54

--- src/assets/js/barnhouse.js
+++ src/assets/js/barnhouse.js
@@ -1,31 +1,30 @@
 function barnFirstScreen() {
-	var headerHeight = $(".site__header").outerHeight();
-	$('.bh__first').css({
-		height: windowHeight - headerHeight
-	});
+  var headerHeight = $('.site__header').outerHeight();
+  $('.bh__first').css({
+    height: windowHeight - headerHeight,
+  });
 }
 
 $(function () {
-	var bhOptions = {};
-	if ($('.bh__container .bh__slide').length) {
-		bhOptions = {
-			slidesPerView: 1,
-			loop: false,
-			spaceBetween: 0,
-			// centeredSlides: true,
-			speed: 600,
-			autoplay: {
-				delay: 15000,
-				disableOnInteraction: true,
-			},
-			navigation: {
-				nextEl: '.bh__right',
-				prevEl: '.bh__left',
-			},
-			keyboard: true,
-			watchOverflow: true,
-
-		};
-	}
-	var swiper = new Swiper('.bh__container', bhOptions);
+  var bhOptions = {};
+  if ($('.bh__container .bh__slide').length) {
+    bhOptions = {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 0,
+      // centeredSlides: true,
+      speed: 600,
+      autoplay: {
+        delay: 15000,
+        disableOnInteraction: true,
+      },
+      navigation: {
+        nextEl: '.bh__right',
+        prevEl: '.bh__left',
+      },
+      keyboard: true,
+      watchOverflow: true,
+    };
+  }
+  var swiper = new Swiper('.bh__container', bhOptions);
 });
